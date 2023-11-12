415525b45ee1e67cb44fb91b141436af34d41d34
Shubham Shah <shubhamshahrising@gmail.com>
2023-11-12 15:45:33 +0530

    Incorrect: entering children also call dragleave on parent

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 // Get button element
 const button = document.getElementById("uploader__button");
 const fileInput = document.getElementById("uploader__input");
+const dropArea = document.getElementById("uploader__drop-area");
+const dropAreaContainer = document.getElementById(
+  "uploader__drop-area-container"
+);
 
 // Add event listener
 button.addEventListener("click", () => {
   fileInput.click();
+});
+// This prevents the default behaviour of the browser opening the file
+document.addEventListener("dragover", (e) => {
+  e.preventDefault();
+});
+
+document.addEventListener("dragenter", (e) => {
+  dropAreaContainer.classList.add("uploader__drop-area-container--active");
+  e.preventDefault();
+});
+document.addEventListener("dragleave", (e) => {
+  dropAreaContainer.classList.remove("uploader__drop-area-container--active");
+  e.preventDefault();
+});
+
+dropArea.addEventListener("drop", (e) => {
+  e.preventDefault();
+  fileInput.files = e.dataTransfer.files;
+  console.log(fileInput.files);
 });
