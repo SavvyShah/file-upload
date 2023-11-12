12dca3a85f9627ce4c270d0b9d250e6e68092354
Shubham Shah <shubhamshahrising@gmail.com>
2023-11-12 12:39:37 +0530

    Trigger a click on input using javascript

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-import "./style.css";
+// Get button element
+const button = document.getElementById("uploader__button");
+const fileInput = document.getElementById("uploader__input");
+
+// Add event listener
+button.addEventListener("click", () => {
+  fileInput.click();
+});
